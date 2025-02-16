2e18e90518a262d0955d851e63d1a63b01efb83d
rahul chavan <rcsoftech85@gmail.com>
2025-02-16 16:12:07 +0530

    tailwindcss

--- laravel/resources/views/user.blade.php
+++ laravel/resources/views/user.blade.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>fetch user</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
-    Hello rahul
+<body class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex items-center justify-center min-h-screen">
+    <div class="max-w-md w-full bg-white dark:bg-gray-300 shadow-lg rounded-lg p-6">
+        <h1 class="text-2xl font-bold mb-4 text-gray-800">Fetch User !</h1>
+        
+        <button class="mt-4 px-4 py-2 bg-yellow-500 text-white rounded-lg hover:bg-blue-400 transition">Click Me</button>
+    </div>
 </body>
 </html>
